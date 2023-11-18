b76778e1e75420f66d9d5c09ceb8fc17e519c1f4
berrtchule <berrtchule@gmail.com>
2023-11-18 17:38:34 +0800

    2023-11-18 17:38:34

--- main.py
+++ main.py
@@ -13,3 +13,4 @@ def shape_list(x):
     """Deal with dynamic shape in tensorflow cleanly."""
     static = x.shape.as_list()
     dynamic = tf.shape(x)
+    return [dynamic[i] if s is None else s for i, s in enumerate(static)]
