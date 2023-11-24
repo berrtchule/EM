916f73cb4de9b18a7ef26a57de3c185457834061
berrtchule <berrtchule@gmail.com>
2023-11-25 03:26:29 +0800

    2023-11-25 03:26:29

--- main.py
+++ main.py
@@ -28,3 +28,4 @@ def norm(x, scope, *, axis=-1, epsilon=1e-5):
     with tf.variable_scope(scope):
         n_state = x.shape[-1].value
         g = tf.get_variable('g', [n_state], initializer=tf.constant_initializer(1))
+        b = tf.get_variable('b', [n_state], initializer=tf.constant_initializer(0))
