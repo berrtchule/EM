3c077ad17ba8b006ded1869ad070427b03342934
berrtchule <berrtchule@gmail.com>
2023-12-06 00:25:44 +0800

    2023-12-06 00:25:44

--- main.py
+++ main.py
@@ -57,3 +57,4 @@ def attention_mask(nd, ns, *, dtype):
     """1's in the lower triangle, counting from the lower right corner.
 
     Same as tf.matrix_band_part(tf.ones([nd, ns]), -1, ns-nd), but doesn't produce garbage on TPUs.
+    """
