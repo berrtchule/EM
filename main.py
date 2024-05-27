c7e64c9f26d973081526a472caddfa1a3b3ea5fa
berrtchule <berrtchule@gmail.com>
2024-05-27 18:47:42 +0800

    2024-05-27 18:47:42

--- main.py
+++ main.py
@@ -156,3 +156,4 @@ def norm(x, scope, *, axis=-1, epsilon=1e-5):
         g = tf.get_variable('g', [n_state], initializer=tf.constant_initializer(1))
         b = tf.get_variable('b', [n_state], initializer=tf.constant_initializer(0))
         u = tf.reduce_mean(x, axis=axis, keepdims=True)
+        s = tf.reduce_mean(tf.square(x-u), axis=axis, keepdims=True)
