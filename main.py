c034a008ef9b2873b232837e497cfcb586e92867
berrtchule <berrtchule@gmail.com>
2024-06-08 02:30:31 +0800

    2024-06-08 02:30:31

--- main.py
+++ main.py
@@ -174,3 +174,4 @@ def merge_states(x):
 def conv1d(x, scope, nf, *, w_init_stdev=0.02):
     with tf.variable_scope(scope):
         *start, nx = shape_list(x)
+        w = tf.get_variable('w', [1, nx, nf], initializer=tf.random_normal_initializer(stddev=w_init_stdev))
