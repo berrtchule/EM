31d68d692934c9da34fdf59b9c8dba70c335d3bc
berrtchule <berrtchule@gmail.com>
2024-11-28 02:09:44 +0800

    2024-11-28 02:09:44

--- main.py
+++ main.py
@@ -317,3 +317,4 @@ def attn(x, scope, n_state, *, past, hparams):
     def multihead_attn(q, k, v):
         # q, k, v have shape [batch, heads, sequence, features]
         w = tf.matmul(q, k, transpose_b=True)
+        w = w * tf.rsqrt(tf.cast(v.shape[-1].value, w.dtype))
