45ff3a8d3794f154ee8393c5523d8f6c3f96811a
berrtchule <berrtchule@gmail.com>
2025-04-08 01:25:13 +0800

    2025-04-08 01:25:13

--- main.py
+++ main.py
@@ -378,3 +378,4 @@ def conv1d(x, scope, nf, *, w_init_stdev=0.02):
         w = tf.get_variable('w', [1, nx, nf], initializer=tf.random_normal_initializer(stddev=w_init_stdev))
         b = tf.get_variable('b', [nf], initializer=tf.constant_initializer(0))
         c = tf.reshape(tf.matmul(tf.reshape(x, [-1, nx]), tf.reshape(w, [-1, nf]))+b, start+[nf])
+        return c
