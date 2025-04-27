f7d8e6f7acb155ced40afd55a7329d187a557a8a
berrtchule <berrtchule@gmail.com>
2025-04-28 01:01:31 +0800

    2025-04-28 01:01:31

--- main.py
+++ main.py
@@ -408,3 +408,4 @@ def attn(x, scope, n_state, *, past, hparams):
     def mask_attn_weights(w):
         # w has shape [batch, heads, dst_sequence, src_sequence], where information flows from src to dst.
         _, _, nd, ns = shape_list(w)
+        b = attention_mask(nd, ns, dtype=w.dtype)
