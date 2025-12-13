f4e86b5a6acde64843b5e7d7fa89ea2b8a0371cd
berrtchule <berrtchule@gmail.com>
2025-12-13 22:40:58 +0800

    2025-12-13 22:40:58

--- main.py
+++ main.py
@@ -482,3 +482,4 @@ def conv1d(x, scope, nf, *, w_init_stdev=0.02):
 def attention_mask(nd, ns, *, dtype):
     """1's in the lower triangle, counting from the lower right corner.
 
+    Same as tf.matrix_band_part(tf.ones([nd, ns]), -1, ns-nd), but doesn't produce garbage on TPUs.
